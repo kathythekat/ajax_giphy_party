bdc4d59e60a6794b1cfc2f7c3d3fda641ade18ae
Kat Huang <khuang129@gmail.com>
2021-02-23 15:58:16 -0800

    disregard first commit, this is first commit

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log("Let's get this party started!");
+
+
+$('form').on('submit', requestGiphy);
+
+async function requestGiphy(e) {
+    e.preventDefault();
+    let gifInput = $('#gifname').val();
+    let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${gifInput}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
+    console.log(response.data.data)
+   
+
+}      
