83cbad24739c090dc7f18491164b2af4c155efa0
Ben Hong <benhong654@gmail.com>
2021-02-23 16:17:41 -0800

    completed assignment :)

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ async function requestGiphy(e) {
     e.preventDefault();
     let gifInput = $('#gifname').val();
     let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${gifInput}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
-    console.log(response.data.data)
-   
+    let dataArr = response.data.data;
+    let randomNum = Math.floor(Math.random() * (dataArr.length-1));
+    let imgUrl = dataArr[randomNum].images.original.url
+    $('#gallery').append(`<img src=${imgUrl}></img>`);
+}     
 
-}      
+$('#remove-button').on('click', () => {
+    $('#gallery').empty();
+})
